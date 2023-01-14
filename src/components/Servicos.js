8dea3412c455374981f1744ae8bab9fa50b298d4
luizgasparetto <luhmgasparetto@gmail.com>
2023-01-14 18:23:59 -0300

    versaoquasefinal

--- src/components/Servicos.js
+++ src/components/Servicos.js
@@ -7,15 +7,19 @@ export default function Servicos() {
     <section className={styles.servicos}>
       <div className={styles.divservicos}>
       <h1>Nossos Serviços</h1>
+      <div>
       <h3>A nossa função consiste em <span>terceirizar
         serviços de radiologia e demais diagnósticos de imagens</span>,
         bem como facilitar as empresas com <span>menos burocracia e menos
           custos e encargos.</span>
       </h3>
+      </div>
+      <div>
       <h3>
         Contamos com uma equipe de profissionais avaliados e
         selecionados na nossa equipe.
       </h3>
+      </div>
       </div>
       <img className={styles.servicoimg} src={terceirizacao} alt="Sobre as vantagens de terceirizar"></img>
     </section>
