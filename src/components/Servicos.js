729eed689a362cb5ab66a3e897db8921b5b2469f
luizgasparetto <luhmgasparetto@gmail.com>
2023-02-07 22:20:34 -0300

    final

--- src/components/Servicos.js
+++ src/components/Servicos.js
@@ -4,17 +4,15 @@ import terc from '../../src/image/terceirizacao.png'
 
 export default function Servicos() {
   return (
-    <div>
-      <div className={styles.servicos}>
+    <div className={styles.servicos}>
+      <div className={styles.servicodiv}>
         <h1>Nossos Serviços</h1>
         <h3>Trabalhamos com a terceirização de serviços em radiologia ou seja, com a mão de
           obra técnica do profissional, gestão de centro de imagem e também com a
           telerradiologia ( laudos à distância ).
         </h3>
       </div>
-      <div className={styles.divimagens}>
-        <img src={terc} alt="terceirização beneficios"></img>
-      </div>
-    </div>
+      <img src={terc} alt="terceirização beneficios"></img>
+    </div >
   )
 }
