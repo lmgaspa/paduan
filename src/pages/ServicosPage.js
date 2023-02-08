9be68c8ce72757535da2f1b551ea9c45f8405288
luizgasparetto <luhmgasparetto@gmail.com>
2023-02-07 23:48:45 -0300

    final

--- src/pages/ServicosPage.js
+++ src/pages/ServicosPage.js
@@ -2,22 +2,23 @@ import React from 'react'
 import PreNavBar from '../components/PreNavBar'
 import Navbar from '../components/Navbar'
 import styles from '../layouts/layoutpages/servicospage.module.css'
-import terceirizacao from '../image/terceirizacao.png'
+import terc from '../image/terceirizacao.png'
 
 export default function ServicosPage() {
   return (
     <section>
       <PreNavBar />
       <Navbar />
-      <div className={styles.containerservicospage}>
-        <div className={styles.containerservicostexto}>
-          <h1>Nossos Serviços</h1>
-          <h3>Trabalhamos com a terceirização de serviços em radiologia ou seja, com a mão de
-            obra técnica do profissional, gestão de centro de imagem e também com a
-            telerradiologia ( laudos à distância ).</h3>
-        </div>
-        <img className={styles.servicopageimg} src={terceirizacao} alt="Sobre as vantagens de terceirizar"></img>
+      <div className={styles.servicos}>
+      <div className={styles.servicodiv}>
+        <h1>Nossos Serviços</h1>
+        <h3>Trabalhamos com a terceirização de serviços em radiologia ou seja, com a mão de
+          obra técnica do profissional, gestão de centro de imagem e também com a
+          telerradiologia ( laudos à distância ).
+        </h3>
       </div>
+      <img src={terc} alt="terceirização beneficios"></img>
+    </div >
     </section>
   )
 }
